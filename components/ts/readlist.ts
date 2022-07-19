34a786e03352054311c4e439f51937c61708fb75
Your Name <you@example.com>
2022-07-20 01:25:14 +0200

    vars

--- components/ts/readlist.ts
+++ components/ts/readlist.ts
@@ -21,14 +21,16 @@ function readlist(props: RestTableParam & ColumnList, f: FSetState) {
 
         .then((rres) => {
             let lres = rres.res
+            let vars = rres.vars
             if (props.method !== undefined) {
                 const da = analyzeresponse(rres.data, rres.response)
                 lres = da[0].res
+                vars = da[0].vars
             }
             transformList(props.columns, { r: {}, t: lres, vars: props.vars })
             const rowkey: string = props.rowkey ? props.rowkey : defaults.rowkeyS
             if (props.rowkey === undefined) addRowKey(lres as RowData, defaults.rowkeyS)
-            f({ status: Status.READY, res: lres, vars: lres.vars, rowkey: rowkey })
+            f({ status: Status.READY, res: lres, vars: vars, rowkey: rowkey })
         }).catch((e) => {
             fatalexceptionerror(`Error while executing ${props.list} ${props.params}`, e)
             f({ status: Status.ERROR, res: [] })
--- ts/defaults.ts
+++ ts/defaults.ts
@@ -23,7 +23,7 @@ const defaults: Defaults = {
   displayprintrouterid: "/printingdisplayer",
   cookieage : 7,
   currentfield: "currentfield",
-  verstring: "Wersja 1.0 (r:13) z dnia 18 lipca 2022 roku",
+  verstring: "Wersja 1.0 (r:14) z dnia 20 lipca 2022 roku",
   directprefix: "~",
   multichoicevar: "multichoice"
 };
