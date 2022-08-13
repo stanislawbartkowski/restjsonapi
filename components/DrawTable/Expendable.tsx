60e8adb97132b5a0aae220bf2608beb0a82cdcd0
Your Name <you@example.com>
2022-08-13 10:27:47 +0200

    changes

--- components/DrawTable/Expendable.tsx
+++ components/DrawTable/Expendable.tsx
@@ -5,12 +5,15 @@ import type { ColumnList, ClickResult, TAction } from "../ts/typing";
 import RestTableList from '../RestTable'
 import type { TRow } from '../../ts/typing'
 import { clickAction } from "../ts/helper";
+import { commonVars } from "../../ts/j";
 
 function getExtendableProps(cols: ColumnList, vars?: TRow): TExtendable {
 
+
   function expand(r: TRow): ReactNode {
+    const rr: TRow = { ...commonVars(), ...r }
     const res: ClickResult = clickAction(cols.extendable as TAction, { r: r, vars: vars });
-    return <RestTableList {...res} vars={r} />
+    return <RestTableList {...res} vars={rr} />
   }
 
   return {
