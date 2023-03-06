0a239326c0f7a8bda5f3708ef9b08f1bcc6b5149
Stanisław Bartkowski <stanislawbartkowski@gmail.com>
2023-03-06 23:48:05 +0100

    recognize port 0

--- ts/init.tsx
+++ ts/init.tsx
@@ -10,7 +10,8 @@ import { initkeyclock } from './keyclock'
 async function init(hostname : string, protocol : string, port : number | undefined) {
     const PORT = (port !== undefined) ? port : await getPagePort()
     console.log(PORT)
-    setHost(`${protocol}//${hostname}:${PORT}`);
+    if (port === 0) setHost(`${protocol}//${hostname}`);
+    else setHost(`${protocol}//${hostname}:${PORT}`);
     await readResource();
     addRouterElem(defaults.displayprintrouterid,<PrintDisplay/>)
     const sec : boolean = await isSecEnabled() 
