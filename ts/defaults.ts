0ccd61cbcd86c1238c2aa0e423712b76e8eedf79
Stanisław Bartkowski <stanislawbartkowski@gmail.com>
2025-01-30 00:20:01 +0100

    text/html

--- ts/defaults.ts
+++ ts/defaults.ts
@@ -49,7 +49,7 @@ const defaults: Defaults = {
   displayprintrouterid: "printingdisplayer",
   cookieage: 7,
   currentfield: "currentfield",
-  verstring: "JS: wersja 1.10 (r:07) z dnia 29 stycznia 2025",
+  verstring: "JS: wersja 1.10 (r:08) z dnia 30 stycznia 2025",
   directprefix: "~",
   multichoicevar: "multichoice",
   listpos: "listpos",
